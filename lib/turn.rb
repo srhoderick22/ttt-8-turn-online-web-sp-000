0db1076e0d30562bb49deefaebdf85d04a80c60f
Stephanie Rhoderick <stephanie.rhoderick@gmail.com>
2019-01-02 16:43:36 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -21,6 +21,21 @@ def valid_move?(board, index)
     end
   end
 
+def on_board?(num)
+  if num.between?(0,8) == true
+    return true
+  else
+    return false
+  end
+end
+
+if (position_taken?(board, index)) == false && (on_board?(index) == true
+  return true
+else
+  return false
+end
+end
+
   def move (board, index, character = "X")
     board [index] = character
     return board
