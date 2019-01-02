341f666ea22da587fe062f179cee44eeffaac3a1
Stephanie Rhoderick <stephanie.rhoderick@gmail.com>
2019-01-02 16:36:44 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -21,7 +21,7 @@ def valid_move?(board, index)
     end
   end
 
-  def move (board, index, character "X")
+  def move (board, index, character = "X")
     board [index] = character
     return board
   end
